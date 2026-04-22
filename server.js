5575d0e99c762c8f83aec8cf6218d7611a6d5b68
RaulPeralta080 <peraltaraulgerardo@gmail.com>
2026-04-21 22:39:49 -0300

    feat: config del server inicial y conexion a mongo

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get('/', (req, res) => {
 // Configuración del puerto
 const PORT = process.env.PORT || 5000;
 
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en modo ${process.env.NODE_ENV} en el puerto ${PORT}`.yellow.bold);
 });
